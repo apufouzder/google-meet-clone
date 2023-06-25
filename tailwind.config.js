7b592ee19527ff07f98eacd98dfd0cdfd2d2f6eb
apufouzder <apufouzdar@gmail.com>
2023-06-25 10:02:05 -0700

    Make Home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primaryColor: "#24A6AF",
+      blackColor: "#17202E",
+      grayColor: "#6B758E"
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
